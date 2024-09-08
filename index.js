c3a55ed4d3b2d73e90caf5af3709e2753ac2d0a5
Akashkr28 <email.akash2010@gmail.com>
2024-09-08 16:27:19 +0530

    chat message event

--- index.js
+++ index.js
@@ -18,6 +18,19 @@ io.on('connection', (socket) => {
     console.log('a user connected');
 })
 
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+});
+
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
 });
